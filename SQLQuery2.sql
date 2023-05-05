57de42bd751de5dbfaa3d0925a8e1d6bf95ccaf0
KoYujung <yujung0435@naver.com>
2023-05-05 23:00:00 +0900

    화면 수정

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -1,49 +1,47 @@
 create table BOOK(
-	BK_CD char(12) not null,
+	BK_CD char(12) not null auto_increment,
 	TITLE char(50),
 	AUTH char(30),
 	PUBL char(30),
 	FIXED int,
 	SELL int,
-	DET char(300),
+	DET varchar(300),
 	IMG file,
 	primary key(BK_CD),
 	foreign key(BKCN_CD) references BOOKCOND(BKCN_CD),
 	foreign key(CATE_ID) references CATE(CATE_ID)
 );
-
 create table BOOKCOND(
 	BKCN_CD char(12),
-	COVER boolean,
-	HIGHPEN boolean,
-	PENCIL boolean,
-	WNAME boolean
-	FPART boolean,
+	COVER char(12),
+	HIGHPEN char(12),
+	PENCIL char(12),
+	WNAME char(12),
+	FPART char(12),
 	primary key(BKCN_CD),
 	foreign key(BK_CD) references BOOK(BK_CD)
 );
-
 create table CATE(
 	CATE_ID char(12),
-	LANG boolean,
-	ECON boolean,
-	COMP boolean,
-	ART boolean,
-	RELI boolean,
+	LANG char(12),
+	ECON char(12),
+	COMP char(12),
+	ART char(12),
+	RELI char(12),
 	primary key(CATE_ID)
 );
-
 create table CHAT(
-
+	CHAT_ID char(12),
+	ID_1
+	primary key(CHAT_ID)
 );
-
-create table ZZIM(
-
-);
-
 create table USER(
 	ID char(20) not null primary key,
 	UNAME char(20),
 	UPASS char(20),
 	TELL char(20)
+);
+create table BOOKMARK(
+	ID char(12),
+	BK_CD char(12)
 );
